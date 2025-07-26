f4429594157540e106c85b2622b21355b1699c72
Vikas Yadav <msvarunvikas24122004@gmail.com>
2025-07-26 23:01:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        int n = arr.length;
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+
+        for (int num : arr) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num : arr) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        Collections.sort(result);
+        return result;
+    }
+}
