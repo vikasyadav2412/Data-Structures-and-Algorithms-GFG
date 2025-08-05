d6a4983914d67bd2a1cd9a51f8b84ce66d333a62
Vikas Yadav <msvarunvikas24122004@gmail.com>
2025-08-05 22:12:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Sentence/palindrome-sentence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalinSent(String s) {
+        // Step 1: Clean the string
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        // Step 2: Check palindrome
+        int left = 0, right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
